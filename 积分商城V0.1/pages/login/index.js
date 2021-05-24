7f595e692ce799de118ccc8f837766fd7a1958d4
Geek-Kevin2333 <80016929+Geek-Kevin2333@users.noreply.github.com>
2021-05-24 17:05:27 +0800

    积分商城V0.1

---
+++ 积分商城V0.1/pages/login/index.js
@@ -0,0 +1,11 @@
+Page({
+  handleGetUserInfo(e){
+    console.log(e)
+    const {userInfo}=e.detail;
+    wx.setStorageSync("userinfo", userInfo);
+    wx.navigateBack({
+      delta: 1
+    });
+      
+  }
+})
