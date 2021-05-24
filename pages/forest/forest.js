3b7ca45e5e58857342b3c5cae93bb1bfa04fdbd4
Mashrio <1109815290@qq.com>
2021-05-25 00:15:56 +0800

    forest随机种树
    实现了随机种树的功能

--- pages/forest/forest.js
+++ pages/forest/forest.js
@@ -13,11 +13,32 @@ Page({
         "/images/myTree2.png",
         "/images/myTree3.png"
       ],  
+      isShow:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],       
+      count:0,
       // check:false
   },
   onClick: function(){
-    console.log("activate!")
-    console.log(this.data.userCredict)
+    var isPlanted = 0
+    var count = this.data.count
+    while(isPlanted == 0 && count<=21)
+    {
+      var randomTree = parseInt(22 * Math.random())
+      if(this.data.isShow[randomTree] == 0)
+      {
+        this.setData({
+        [`isShow[${randomTree}]`]:1,
+        })
+        isPlanted=1
+      }
+    }
+      count++
+      this.setData({
+        count:count
+      })
+    console.log("this date count is " + count)
+    if(count > 30)
+      console.log("there is some errors")
+    this.onLoad
   },
   /**
    * 生命周期函数--监听页面加载
