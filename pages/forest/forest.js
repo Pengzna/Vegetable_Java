340475ccef84f8616fbb16dc8a802a432260fc2f
Pengzna <201250214@smail.nju.edu.cn>
2021-05-24 16:56:02 +0800

    Initial Commit

---
+++ pages/forest/forest.js
@@ -0,0 +1,75 @@
+// pages/forest.js
+const App = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      userCredict : App.globalData.userCredict,
+      icon:[
+        "/images/myTree1.png",
+        "/images/myTree2.png",
+        "/images/myTree3.png"
+      ],  
+  },
+  onClick: function(){
+    console.log("activate!")
+    console.log(this.data.userCredict)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  // onShow: function () {
+
+  // },
+
+  // /**
+  //  * 生命周期函数--监听页面隐藏
+  //  */
+  // onHide: function () {
+
+  // },
+
+  // /**
+  //  * 生命周期函数--监听页面卸载
+  //  */
+  // onUnload: function () {
+
+  // },
+
+  // /**
+  //  * 页面相关事件处理函数--监听用户下拉动作
+  //  */
+  // onPullDownRefresh: function () {
+
+  // },
+
+  // /**
+  //  * 页面上拉触底事件的处理函数
+  //  */
+  // onReachBottom: function () {
+
+  // },
+
+  // /**
+  //  * 用户点击右上角分享
+  //  */
+  // onShareAppMessage: function () {
+
+  // }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,96 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  nLoad: function (options) {
+    
+  },
+  data: {
+    imgSources: [
+      '/images/SwiperXi.png',
+      '/images/SwiperForest.jpg',
+      '/images/Swiper2.jpg'
+    ],
+    indicatorDots: true,  //是否显示面板指示点
+    autoplay: true,      //是否自动切换
+    interval: 3000,       //自动切换时间间隔
+    duration: 1000,       //滑动动画时长
+    inputShowed: false,
+    active: 1,
+    
+    // inputVal: ""
+    // motto: 'Hello World',
+    // userInfo: {},
+    // hasUserInfo: false,
+    // canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // canIUseGetUserProfile: false,
+    // canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  },
+  onClickSociety:function(){
+    //nevigateTo
+    console.log("execute!")
+  },
+  onClickShop:function(){
+    //nevigateTo
+    console.log("execute!")
+  },
+  onClickForest:function(){
+    //nevigateTo
+    console.log("execute!")
+  },
+  onClickContest:function(){
+    //nevigateTo
+    console.log("execute!")
+  },
+  onChange(event) {
+    wx.showToast({
+      title: `切换到${event.detail.title}`,
+      icon: 'none',
+    });
+  },
+
+
+
+
+
+
+
+
+
+
+  // 事件处理函数
+  // bindViewTap() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  // onLoad() {
+  //   if (wx.getUserProfile) {
+  //     this.setData({
+  //       canIUseGetUserProfile: true
+  //     })
+  //   }
+  // },
+  // getUserProfile(e) {
+  //   // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+  //   wx.getUserProfile({
+  //     desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+  //     success: (res) => {
+  //       console.log(res)
+  //       this.setData({
+  //         userInfo: res.userInfo,
+  //         hasUserInfo: true
+  //       })
+  //     }
+  //   })
+  // },
+  // getUserInfo(e) {
+  //   // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+  //   console.log(e)
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // }
+})
